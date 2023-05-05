061ec0d499fc88bee8958aa411da0fad6a189304
Zani231089 <231089@virtualwindow.co.za>
2023-05-05 16:57:21 +0200

    Final

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let firstName = "Zani"
+let surname = "van den Bosch"
+let age = 19
+let subject = "Creative Technologies"
+
+let message = "Hello, my name is " + firstName + " " + surname + "," + "I am " + age + " years old and I am studiying " + subject;
+
+console.log(message)
