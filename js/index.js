321c2d69ef10affa3cc5c8ddede1401b53dacf9c
Zani231089 <231089@virtualwindow.co.za>
2023-05-12 15:31:41 +0200

    Final

--- js/index.js
+++ js/index.js
@@ -6,3 +6,59 @@ let subject = "Creative Technologies"
 let message = "Hello, my name is " + firstName + " " + surname + "," + "I am " + age + " years old and I am studiying " + subject;
 
 console.log(message)
+
+let numOne = 12;
+let numTwo = 20;
+let numThree = 4;
+let numFour = 0;
+
+let teamTotal = numOne + numTwo;
+let victory = teamTotal / numThree;
+let total = victory * numFour;
+
+let answer = "The answer is " + total;
+
+console.log(answer)
+
+let timmy = 10;
+let taxmanOne = 15;
+let percentageOne = 100;
+
+let groupOne = timmy * taxmanOne;
+let victoryOne = groupOne / percentageOne;
+
+let answerOne = "Timmy owes the taxman " + "R" + victoryOne;
+
+console.log(answerOne)
+
+let susan = 25;
+let taxmanTwo = 26;
+let percentageTwo = 100;
+
+let groupTwo = susan * taxmanTwo;
+let victoryTwo = groupTwo / percentageTwo;
+
+let answerTwo = "Susan owes the Taxman " + "R" + victoryTwo;
+
+let answerThree = victoryOne + victoryTwo;
+let finalAnswer = "The taxman will claim " + "R" + answerThree + " from Timmy and Susan"
+
+console.log(finalAnswer)
+
+let john = 1;
+let Tim = 2;
+let alfie = 3;
+let gwen = 4;
+let constant = 4;
+let test = 100;
+
+let value = john / constant;
+
+let resultOne = "The student average is " + value + "/100"
+console.log(resultOne);
+
+let newValue = 0.07;
+let value2 = value + newValue;
+
+let result2 = "The new student average is " + value2 + "/100";
+console.log(result2);
